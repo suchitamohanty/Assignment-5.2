afec5143f46b3e81921cff51152614b66e10629b
Suchita Mohanty <mohanty.suchita@gmail.com>
2017-01-23 08:44:16 +0530

    Create an object using object literal notation to display Name, Age, Dob and Place of birth

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Assignment –Create an object using object literal notation that
+stores your name, age, dob and placeofbirth. Display the value
+of each property in console.
---
+++ js/app.js
@@ -0,0 +1,13 @@
+var mydetail = {
+	name: 'Suchita',
+	age: 30,
+	dob: new Date('06-19-1986'),
+	pob: 'Odisha',
+};
+//document.getElementById("demo").innerHTML	= me.name + " is " +  me.age  + " year old, " +  me.dob  + " is her date of birth and "  +  me.pob  +  " is her place of birth.",
+console.log ('may detail:');
+console.log ('Name: ' + mydetail.name);
+console.log ('Age: ' + mydetail.age);
+console.log ('DOB: ' + mydetail.dob);
+console.log ('POB: ' + mydetail.pob);
+
